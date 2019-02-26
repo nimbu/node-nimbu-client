657dcb696d7bc05f4fb1914ebd0de5390c1a411c
Peter Dedene <peter@zenjoy.be>
2019-02-26 21:42:19 +0100

    Initial version of a simple Node.js Nimbu Client

---
+++ README.md
@@ -0,0 +1,79 @@
+# nimbu-client
+
+A thin wrapper around the [Nimbu](https://www.nimbu.io) API.
+
+- [nimbu-client](#nimbu-client)
+  - [Install](#install)
+  - [Usage](#usage)
+    - [HTTP Proxies](#http-proxies)
+    - [Running tests](#running-tests)
+
+## Install
+
+```sh
+$ npm install nimbu-client --save
+```
+
+## Usage
+
+To begin, require the Nimbu module and create the client, passing in an API
+token:
+
+```javascript
+const Nimbu = require('nimbu-client');
+const nimbu = new Nimbu({ token: apiToken });
+```
+
+If a customer session token is needed, you can pass this too:
+
+```javascript
+const nimbu = new Nimbu({ token: apiToken, sessionToken: sessionToken });
+```
+
+nimbu-client has `get`, `post`, `patch`, and `delete` functions which can make
+requests with the specified HTTP method to any endpoint:
+
+```javascript
+// GET requests
+nimbu.get('/channels').then(channels => {
+  // do something with channel info
+});
+
+// POST requests with body
+nimbu.post('/channels', { body: { name: 'foo' } }).then(channel => {});
+
+// PATCH requests with body
+nimbu.patch('/channels/foo', { body: { name: 'bar' } }).then(channel => {});
+
+// DELETE requests
+nimbu.delete('/channels/bar').then(channel => {});
+```
+
+There is also an even more generic `request` function that can accept many more
+options:
+
+```javascript
+nimbu
+  .request({
+    method: 'GET',
+    path: '/channels',
+    parseJSON: false,
+  })
+  .then(response => {});
+```
+
+### HTTP Proxies
+
+If you'd like to make requests through an HTTP proxy, set the
+`NIMBU_HTTP_PROXY_HOST` environment variable with your proxy host, and
+`NIMBU_HTTP_PROXY_PORT` with the desired port (defaults to 8080). nimbu-client
+will then make requests through this proxy instead of directly to
+api.nimbu.io.
+
+### Running tests
+
+nimbu-client uses [ava](https://github.com/avajs/ava) for tests:
+
+```bash
+$ npm test
+```
---
+++ test.js
@@ -0,0 +1,94 @@
+'use strict';
+
+import test from 'ava';
+import nock from 'nock';
+import Nimbu from '.';
+
+let nimbu = new Nimbu();
+
+test.before(() => {
+  nock.disableNetConnect();
+});
+
+test('get /channels', t => {
+  let api = nock('https://api.nimbu.io')
+    .get('/channels')
+    .reply(200, [{ name: 'foo' }]);
+
+  return nimbu
+    .get('/channels')
+    .then(apps => {
+      t.is(apps[0].name, 'foo');
+    })
+    .then(() => t.true(api.isDone()));
+});
+
+test('get /channels with pagination', t => {
+  let api1 = nock('https://api.nimbu.io')
+    .get('/channels')
+    .reply(200, [{ name: 'foo' }], {
+      Link:
+        '<https://api.nimbu.io/channels?page=2>; rel="next", <https://api.nimbu.io/channels?page=2>; rel="last"',
+    });
+
+  let api2 = nock('https://api.nimbu.io')
+    .get('/channels')
+    .query({ page: 2 })
+    .reply(200, [{ name: 'bar' }], {
+      Link:
+        '<https://api.nimbu.io/channels?page=1>; rel="prev", <https://api.nimbu.io/channels?page=1>; rel="first"',
+    });
+
+  return nimbu
+    .get('/channels', { fetchAll: true })
+    .then(apps => {
+      t.is(apps[0].name, 'foo');
+      t.is(apps[1].name, 'bar');
+    })
+    .then(() => t.true(api1.isDone() && api2.isDone()));
+});
+
+test('post /channels', t => {
+  let api = nock('https://api.nimbu.io')
+    .post('/channels', { name: 'foo' })
+    .reply(201);
+
+  return nimbu.post('/channels', { body: { name: 'foo' } }).then(() => t.true(api.isDone()));
+});
+
+test('patch /channels', t => {
+  let api = nock('https://api.nimbu.io')
+    .patch('/channels', { name: 'foo' })
+    .reply(201);
+
+  return nimbu.patch('/channels', { body: { name: 'foo' } }).then(() => t.true(api.isDone()));
+});
+
+test('put /channels', t => {
+  let api = nock('https://api.nimbu.io')
+    .put('/channels', { name: 'foo' })
+    .reply(201);
+
+  return nimbu.put('/channels', { body: { name: 'foo' } }).then(() => t.true(api.isDone()));
+});
+
+test('delete /channels', t => {
+  let api = nock('https://api.nimbu.io')
+    .delete('/channels', { name: 'foo' })
+    .reply(201);
+
+  return nimbu.delete('/channels', { body: { name: 'foo' } }).then(() => t.true(api.isDone()));
+});
+
+test('non-http', t => {
+  let api = nock('http://api.nimbu.io')
+    .get('/channels')
+    .reply(200, [{ name: 'foo' }]);
+
+  return nimbu
+    .get('/channels', { host: 'http://api.nimbu.io' })
+    .then(apps => {
+      t.is(apps[0].name, 'foo');
+    })
+    .then(() => api.isDone());
+});
